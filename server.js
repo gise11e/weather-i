224b023764032f6140c3fc3ca8f42dbf7a6e4cfb
gi <gise11e@me.com>
2017-01-10 16:26:05 +0000

    deploy

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ var express = require('express');
 //create ap
 
 var app = express();
+const port  =   process.env.PORT || 3000;
 
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  }else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 app.use(express.static('public'));
 
-app.listen(3000, function (){
-  console.log('express server is up');
+app.listen(port, function (){
+  console.log('express server is up' +  port);
 });
